51dab49d0f25bdde02afa9b68b4988d7907e4fa4
yurigligoric <yurigligoric@gmail.com>
2024-02-01 11:35:15 +0800

    first commit setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Test from './components/test'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline text-red-800'>Hello World!</h1>
+    </>
+  )
+}
+
+export default App
